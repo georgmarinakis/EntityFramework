e4feef6a74772515e5758828147f7e18e61fde7b
gmarinakis <g.marinakis@outlook.com>
2020-08-21 13:47:50 +0300

    First commit

---
+++ CodeFirst/CodeFirst/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+
+    public class Course
+    {
+        public int Id { get; set; } //key
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+        public CourseLevel Level { get; set; } // navigation property
+
+        public float FullPrice { get; set; }
+
+        public Author Author { get; set; } // navigation property
+
+        public IList<Tag> Tags { get; set; } //one-to-many
+    }
+}
